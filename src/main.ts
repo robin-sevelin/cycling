afd725f62e16cc69a6a5acba50693365bc1e2da3
robin-sevelin <robinsevelin@hotmail.com>
2023-02-26 11:42:04 +0100

    merge dev to main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-import './style.css';
+import './style/style.css';
+
+const hamburger: HTMLElement = document.querySelector(
+  '.hamburger'
+) as HTMLElement;
+const navMenu: HTMLElement = document.querySelector('.nav-menu') as HTMLElement;
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
